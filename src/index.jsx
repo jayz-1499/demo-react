bdc0d73a56512652f7228c99e8b97580739235a5
DanHN <hanhatdan@gmail.com>
2021-09-21 14:11:12 +0700

    update

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+index.propTypes = {
+  number: PropTypes.array,
+};
+
+function index(props) {
+  return <div></div>;
+}
+
+export default index;
